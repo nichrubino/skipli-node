c5e12949fa3beaea6600bb376510aa4d4c3c050d
nichrubino <github@nichrubino.it>
2026-02-01 00:34:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 const fs = require('fs');
-const crypto = require('crypto');
 const WebSocket = require('ws');
 
-/* ===== CONFIG ===== */
-
-const CONFIG = {
+var CONFIG = {
   port: 8080,
   maxDiskBytes: 20 * 1024 * 1024 * 1024,
   maxUrlLength: 100000,
   dbFile: './links.db.json'
 };
-
-/* ===== STORAGE ===== */
 
 if (!fs.existsSync(CONFIG.dbFile)) {
   fs.writeFileSync(CONFIG.dbFile, JSON.stringify({ totalBytes: 0, links: [] }));
@@ -24,24 +19,27 @@ function saveDb() {
 }
 
 function enforceLimit() {
-  db.links.sort((a, b) => a.created - b.created);
+  db.links.sort(function(a,b){ return a.created - b.created });
   while (db.totalBytes > CONFIG.maxDiskBytes && db.links.length) {
-    const old = db.links.shift();
+    var old = db.links.shift();
     db.totalBytes -= old.size;
   }
 }
 
-function addLink(alias, url) {
+function addLink(alias, url, hash) {
   if (url.length > CONFIG.maxUrlLength) {
     url = url.substring(0, CONFIG.maxUrlLength);
   }
 
-  const size = Buffer.byteLength(url);
+  var size = Buffer.byteLength(url);
+
+  db.links = db.links.filter(function(l){ return l.alias !== alias });
 
   db.links.push({
-    alias,
-    url,
-    size,
+    alias: alias,
+    url: url,
+    hash: hash || null,
+    size: size,
     created: Date.now()
   });
 
@@ -51,30 +49,32 @@ function addLink(alias, url) {
 }
 
 function getLink(alias) {
-  return db.links.find(l => l.alias === alias);
+  return db.links.find(function(l){ return l.alias === alias });
 }
 
-/* ===== SERVER ===== */
+var wss = new WebSocket.Server({ port: CONFIG.port });
 
-const wss = new WebSocket.Server({ port: CONFIG.port });
-
-wss.on('connection', ws => {
-  ws.on('message', msg => {
-    let data;
+wss.on('connection', function(ws) {
+  ws.on('message', function(msg) {
+    var data;
     try { data = JSON.parse(msg); } catch { return; }
 
-    if (data.type === 'store') {
-      addLink(data.alias, data.url);
+    if (data.type === 'new') {
+      if (!data.alias || !data.url) return;
+      addLink(data.alias, data.url, data.hash);
       ws.send(JSON.stringify({ type: 'stored', alias: data.alias }));
+      return;
     }
 
     if (data.type === 'get') {
-      const link = getLink(data.alias);
+      var link = getLink(data.alias);
       ws.send(JSON.stringify({
         type: 'result',
         alias: data.alias,
-        url: link ? link.url : null
+        url: link ? link.url : null,
+        hash: link ? link.hash : null
       }));
+      return;
     }
   });
 });
